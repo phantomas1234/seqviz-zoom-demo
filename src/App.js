f2bbc1bf136fa775f0ea2b0ba779115591d55f04
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2022-07-14 11:14:54 +0200

    Add zoom input field and large genbank files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import "./App.css";
 import React from "react";
-import { SeqViz } from "seqviz";
+import { SeqViz } from "@phantomas1234/seqviz";
 
 function App() {
   const [file, setFile] = React.useState("");
+  const [zoom, setZoom] = React.useState(50);
 
   React.useEffect(() => {
-    fetch("/sequence.gb")
+    fetch("/sequence_small.gb")
       .then((r) => r.text())
       .then((text) => {
         console.log("text decoded:", text);
         setFile(text);
       });
-  });
+  }, []);
 
   return (
     <div className="App">
       <div className="container w-max h-max">
-        <SeqViz file={file} style={{ height: "100vh", width: "100vw" }} />
+        <div className="bg-slate-50">
+          <form>
+            <input
+              type="text"
+              onChange={(e) => {
+                console.log(parseInt(e.target.value));
+                setZoom(parseInt(e.target.value));
+              }}
+            ></input>
+          </form>
+        </div>
+        <SeqViz
+          file={file}
+          style={{ height: "100vh", width: "100vw" }}
+          zoom={{ linear: zoom, circular: 50 }}
+        />
       </div>
     </div>
   );
